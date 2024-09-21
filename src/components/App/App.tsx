aa84d7f92a7b46c104014007fca4a0d649a42866
Raleigh, CA <knicholson@taoshi.io>
2024-09-20 23:28:11 -0400

    add error boundary and fallback

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,3 @@
-import { isEmpty } from "lodash";
 import { useState, useEffect } from "react";
 import { AppShell, Center, Loader } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -6,6 +5,8 @@ import { useDisclosure } from "@mantine/hooks";
 import { MinerData } from "../../types";
 import { getMinerData } from "../../lib";
 
+import { ErrorBoundary } from "../ErrorBoundary";
+import { ErrorFallback } from "../ErrorFallback";
 import { AppHeader } from "../AppHeader";
 import { Main } from "../Main";
 
@@ -29,6 +30,7 @@ export const App = () => {
       } catch (error: unknown) {
         if (error instanceof Error) {
           setError(error.message);
+          throw new Error(error.message);
         } else {
           setError("An unknown error occurred");  // Optional: Handle non-Error objects
         }
@@ -48,36 +50,23 @@ export const App = () => {
     );
   }
   
-  if (error) {
-    return (
-      <Center>
-        Error: {error}
-      </Center>
-    );
-  }
-  
-  if (isEmpty(data?.positions) || isEmpty(data?.statistics)) {
-    return (
-      <Center>
-        No data available.
-      </Center>
-    );
-  }
   
   return (
-    <AppShell
-      navbar={{ width: 300, breakpoint: "sm", collapsed: { desktop: true, mobile: !opened } }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <AppHeader opened={opened} toggle={toggle} data={data?.statistics} />
-      </AppShell.Header>
-      
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-      
-      <AppShell.Main>
-        <Main data={data as MinerData} />
-      </AppShell.Main>
-    </AppShell>
+    <ErrorBoundary fallback={<ErrorFallback error={new Error(error as string)} />}>
+      <AppShell
+        navbar={{ width: 300, breakpoint: "sm", collapsed: { desktop: true, mobile: !opened } }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <AppHeader opened={opened} toggle={toggle} data={data?.statistics} />
+        </AppShell.Header>
+        
+        <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+        
+        <AppShell.Main>
+          <Main data={data as MinerData} />
+        </AppShell.Main>
+      </AppShell>
+    </ErrorBoundary>
   );
 };
